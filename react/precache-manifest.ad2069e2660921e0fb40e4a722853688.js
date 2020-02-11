0c0e88781da0eb52ec5ddbc70f07dc15985b906e
Jason Hampleman <>
2020-02-11 10:12:17 -0700

    React - Fix Homepage URL issue

---
+++ react/precache-manifest.ad2069e2660921e0fb40e4a722853688.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc0d80060f673e5b2ad72f36014e1f05",
+    "url": "/webdev-sample-qa/react/index.html"
+  },
+  {
+    "revision": "0064cebda418faa663b5",
+    "url": "/webdev-sample-qa/react/static/css/main.ab0a9413.chunk.css"
+  },
+  {
+    "revision": "7681097e56a2b6e45099",
+    "url": "/webdev-sample-qa/react/static/js/2.c41685d5.chunk.js"
+  },
+  {
+    "revision": "c6967699ed8f6cf33744a1f8a8d37ba5",
+    "url": "/webdev-sample-qa/react/static/js/2.c41685d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0064cebda418faa663b5",
+    "url": "/webdev-sample-qa/react/static/js/main.fe46753f.chunk.js"
+  },
+  {
+    "revision": "765de8ed91d48831e4f1",
+    "url": "/webdev-sample-qa/react/static/js/runtime-main.ce40c2c5.js"
+  },
+  {
+    "revision": "b0ab2b36c8c276a1584e570f3db873c9",
+    "url": "/webdev-sample-qa/react/static/media/unineue-book-webfont.b0ab2b36.ttf"
+  },
+  {
+    "revision": "ce94bc79b71c79e6144a42ec2c539458",
+    "url": "/webdev-sample-qa/react/static/media/unineue-book-webfont.ce94bc79.eot"
+  },
+  {
+    "revision": "454c512fe5d0cef211e7823353360f7f",
+    "url": "/webdev-sample-qa/react/static/media/viasat-logo.454c512f.svg"
+  }
+]);
--- react/service-worker.js
+++ react/service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/webdev-sample-qa/react/precache-manifest.0717f9dd0d5afd335f4ef0145b7f38ed.js"
+  "/webdev-sample-qa/react/precache-manifest.ad2069e2660921e0fb40e4a722853688.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/webdev-sample-qa/react/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/webdev-sample-qa/react/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
