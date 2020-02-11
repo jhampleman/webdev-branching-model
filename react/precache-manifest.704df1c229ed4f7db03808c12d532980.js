fe319d61e84ec528f022c57da82ee849cbd03648
Jason Hampleman <>
2020-02-11 11:28:02 -0700

    Post htaccess fix

---
+++ react/precache-manifest.704df1c229ed4f7db03808c12d532980.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa63c3fe8c0bede795181b727104aa8d",
+    "url": "/webdev-sample-qa/react/index.html"
+  },
+  {
+    "revision": "0aa7941c68375a23e1ba",
+    "url": "/webdev-sample-qa/react/static/css/main.ab0a9413.chunk.css"
+  },
+  {
+    "revision": "7681097e56a2b6e45099",
+    "url": "/webdev-sample-qa/react/static/js/2.c41685d5.chunk.js"
+  },
+  {
+    "revision": "c6967699ed8f6cf33744a1f8a8d37ba5",
+    "url": "/webdev-sample-qa/react/static/js/2.c41685d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0aa7941c68375a23e1ba",
+    "url": "/webdev-sample-qa/react/static/js/main.b9f34680.chunk.js"
+  },
+  {
+    "revision": "765de8ed91d48831e4f1",
+    "url": "/webdev-sample-qa/react/static/js/runtime-main.ce40c2c5.js"
+  },
+  {
+    "revision": "b0ab2b36c8c276a1584e570f3db873c9",
+    "url": "/webdev-sample-qa/react/static/media/unineue-book-webfont.b0ab2b36.ttf"
+  },
+  {
+    "revision": "ce94bc79b71c79e6144a42ec2c539458",
+    "url": "/webdev-sample-qa/react/static/media/unineue-book-webfont.ce94bc79.eot"
+  },
+  {
+    "revision": "454c512fe5d0cef211e7823353360f7f",
+    "url": "/webdev-sample-qa/react/static/media/viasat-logo.454c512f.svg"
+  }
+]);
